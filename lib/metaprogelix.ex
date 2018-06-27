7f1f7572e0570ddac74c9b75f93b5f9daf91acee
Ricardo C <ricardo@suggestic.com>
2018-06-27 17:22:55 -0500

    add codebase

---
+++ lib/metaprogelix.ex
@@ -0,0 +1,18 @@
+defmodule Metaprogelix do
+  @moduledoc """
+  Documentation for Metaprogelix.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Metaprogelix.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/metaprogelix_test.exs
@@ -0,0 +1,8 @@
+defmodule MetaprogelixTest do
+  use ExUnit.Case
+  doctest Metaprogelix
+
+  test "greets the world" do
+    assert Metaprogelix.hello() == :world
+  end
+end
