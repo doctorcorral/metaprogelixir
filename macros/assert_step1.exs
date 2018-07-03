068123a509f8ef564c0342b1408e98c6fd2037ad
Ricardo C <ricardo@suggestic.com>
2018-07-02 19:54:39 -0500

    add Assertion module

---
+++ macros/assert_step1.exs
@@ -0,0 +1,7 @@
+defmodule Assertion do
+  defmacro assert({operator, _, [lhs, rhs]}) do
+    quote bind_quoted: [operator: operator, lhs: lhs, rhs: rhs] do
+      Assertion.Test.assert(operator, lhs, rhs)
+    end
+  end
+end
