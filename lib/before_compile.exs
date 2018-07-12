02067e6796370a4489880a81d0be07f880607cc2
Ricardo C <ricardo@suggestic.com>
2018-07-12 18:45:33 -0500

    add _using_ and __before_compile__ macros

---
+++ lib/before_compile.exs
@@ -0,0 +1,19 @@
+defmodule Assertion do
+  defmacro __using__(_options) do
+    quote do
+      import unquote(__MODULE__)
+      Module.register_attribute(__MODULE__, :tests, accumulate: true)
+      @before_compile unquote(__MODULE__)
+    end
+  end
+
+  defmacro __before_compile__(_env) do
+    quote do
+      def run do
+        IO.puts "Corriendo los test"
+      end
+    end
+
+  end
+
+end
