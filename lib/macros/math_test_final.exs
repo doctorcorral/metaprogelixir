d26fc71c8c39f1ebcbbf8060eb403a9261ba3907
Ricardo C <ricardo@suggestic.com>
2018-07-18 19:02:17 -0500

    add test for integer division

--- lib/macros/math_test_final.exs
+++ lib/macros/math_test_final.exs
@@ -1,8 +1,13 @@
 defmodule MathTest do
   use Assertion
 
-  test "integers can be added and substracted" do
+  test "los enteros se pueden restar" do
     assert 2 + 2 == 5
     assert 5 - 5 == 10
+  end
+
+  test "los enteros se pueden dividir" do
+    assert 5 * 5 == 25
+    assert 10 / 2 == 5
   end
 end
