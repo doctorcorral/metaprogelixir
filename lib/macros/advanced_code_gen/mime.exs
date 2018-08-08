47bc35a76b869cbebdd4028200bb803007bce082
Ricardo C <ricardo@suggestic.com>
2018-08-08 17:51:04 -0500

    add clauses and valid_type?()

--- lib/macros/advanced_code_gen/mime.exs
+++ lib/macros/advanced_code_gen/mime.exs
@@ -5,4 +5,8 @@ defmodule Mime do
     def exts_from_type(unquote(type)), do: unquote(extensions)
     def type_from_text(ext) when ext in unquote(extensions), do: unquote(type)
   end
+
+  def exts_from_type(_type), do: []
+  def type_from_ext(_ext), do: nil
+  def valid_type?(type), do: exts_from_type(type) |> Enum.any?()
 end
